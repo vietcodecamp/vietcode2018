8d72775b243aba5d1353bf29eeb2242e05278f37
Dagy Tran <dagytran@gmail.com>
2019-08-10 13:02:52 +0200

    Removing lessons 2-6

--- Exercises/Lesson_2/prevod_na_velka_pismeny.py
+++
@@ -1,9 +0,0 @@
-# Upper Case
-# Napište program, který na tebe zakřičí to, co jsi napsal.
-# Nápověda: - použij funkci .upper()
-
-# 1. získej vstup
-
-# 2. převést na velká písmena
-
-# 3. tisk výsledku
--- Exercises/Lesson_4/jidlo2.py
+++
@@ -1,6 +0,0 @@
-jidla = []
-for i in range(5):
-    # 1. získej hodnotu jídla od uživatele a připoj ho do listu jidla
-    input()
-
-# 2. Vytiskni odpověď  - pro tisk všechno hodnot v listu jidla použij: ', '.join(jidla)
